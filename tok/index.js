f746849a99b68b4c878d5aeb07007f126dd447ca
jkbits1 <jkbits1@users.noreply.github.com>
2016-02-12 02:47:20 +0000

    get token

--- tok/index.js
+++ tok/index.js
@@ -21,12 +21,18 @@
     var sessionId;
     //opentok.createSession(null, {mediaMode:"routed"}, function(error, session) {
     opentok.createSession({mediaMode:"routed"}, (error, session) => {
+      var token = undefined;
+
       if (error) {
         return console.log("Error creating session:", error);
       }
 
       sessionId = session.sessionId;
       console.log("Session ID: " + sessionId);
+
+      token = session.generateToken();
+
+      console.log(token);
     });
   });
 })();
